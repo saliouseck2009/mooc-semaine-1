609f5f4ae0b1915796fb803d77e755e6670813f4
saliouseck2009 <39632390+saliouseck2009@users.noreply.github.com>
2021-04-01 14:19:22 +0000

    derniere etape

---
+++ WebContent/WEB-INF/bag.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html"
+         pageEncoding="UTF-8" %>
+<%@ page import="controller.Bag" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Insert title here</title>
+</head>
+<body>
+
+<h1>Sac</h1>
+
+
+
+    <form method='POST' action='home'>
+        <label for='ref'>Ref</label>
+        <input type='text' id='ref' name='ref'/><br/>
+        <label for='qty'>Qty</label>
+        <input type='text' id='qty' name='qty'/><br/>
+        <input type='submit' value='send'>
+    </form>
+
+</body>
+</html>
--- src/controller/BagServlet.java
+++ src/controller/BagServlet.java
@@ -13,53 +13,32 @@ import javax.servlet.http.*;
 // TODO: add an annotation here to map your servlet on an URL.
 @WebServlet("/home")
 public class BagServlet extends HttpServlet{
+
+	public static String jspView = "/WEB-INF/bag.jsp";
 	
 	
+//	@Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println( "In the service method" );
+//        super.service(req, resp);
+//    }
 	@Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println( "In the service method" );
-        super.service(req, resp);
-    }
-	
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
 		
-	HttpSession session = req.getSession();
-	System.out.println("in the do GET");
-	String login = req.getParameter( "txtLogin" );
-    String password = req.getParameter( "txtPassword" );
-    if ( login == null ) login = "";
-    if ( password == null ) password = "";
-	res.setContentType("text/html");
-	
-		try (PrintWriter out = res.getWriter()){
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Test</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<h1> Contenue du panier <h1/>");
-		if(session.getAttribute("myBag")!=null) {
-			Bag mynewBag = (Bag) session.getAttribute("myBag");
-			mynewBag.print(out);
-		}
-		
-		out.println("<form method='POST' action='home'>" );
-		out.println("<label for='ref'>Ref</label>");
-		out.println("<input type='text' id='ref' name='ref' /><br/>");
-		out.println("<label for='qty'>Qty</label>");
-		out.println( "<input type='text id='qty' name='qty' /><br/>");
-		out.println( "<input type='submit' value='send' >");
-		out.println( "</form>");
-		out.println("</body>");
-		out.println("</html>");
+//	HttpSession session = req.getSession();
+//	System.out.println("in the do GET");
+//	String login = req.getParameter( "txtLogin" );
+//    String password = req.getParameter( "txtPassword" );
+//    if ( login == null ) login = "";
+//    if ( password == null ) password = "";
+		this.getServletContext().getRequestDispatcher(jspView).forward(req, res);
 
-		}
+
 		
 	}
 
 	
-	
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
 	throws ServletException, IOException {
 		HttpSession session = req.getSession();
@@ -82,7 +61,8 @@ public class BagServlet extends HttpServlet{
 		try {
 			
 			myBag.setItem(ref, Integer.parseInt(qty));
-			doGet(req,res);
+			req.setAttribute("myBag",myBag);
+			this.getServletContext().getRequestDispatcher(jspView).forward(req, res);
 		} catch (NumberFormatException e) {
 			e.getMessage();
 			doGet(req,res);
